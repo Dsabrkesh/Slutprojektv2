b1ef2c56cd85e3189916f946ef5287b2bea3ac0f
Petrov2g <dimsipecrov@gmail.com>
2019-03-28 10:17:19 +0100

    shopv0.1

---
+++ home.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Shop - MazeClan</title>
+	<link rel="icon" type="images/png" href="images/favicon.png" sizes="32x32"/>
+	<link rel="stylesheet" type="text/css" href="css/shop.css">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta charset="utf-8">
+</head>
+<body>
+
+
+
+</body>
+</html>

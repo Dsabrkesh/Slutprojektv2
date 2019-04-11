d887faef4db4fd861376f8cb88d032b861687967
Dsabrkesh <42805455+Dsabrkesh@users.noreply.github.com>
2019-04-11 09:26:59 +0200

    Add files via upload

--- db.sql
+++ db.sql
@@ -1,14 +1,3 @@
--- PRODUCT TABLE 
-CREATE TABLE product(
-	id int(8) PRIMARY KEY AUTO_INCREMENT,
-	productname varchar(32),
-	price varchar(32),
-	size varchar(32)
-)
-
-INSERT INTO `product` (`id`, `productname`, `price`, `size`) VALUES (NULL, 'MazeClan2019 TShirt', '30$', 'XL'), (NULL, 'MazeClan2019 TShirt', '25$', 'XXL');
-INSERT INTO `product` (`id`, `productname`, `price`, `size`, `product_image`) VALUES (NULL, 'MazeClan 2019 Logo Hoodie ', '50$', 'XL', 'images/products/shirt.png');
-INSERT INTO `product` (`id`, `productname`, `price`, `size`, `product_image`) VALUES (NULL, 'MazeClan 2019 Logo Hoodie Black', '50$', 'XL', 'images/products/shirt1.png');
 
 CREATE TABLE accounts(
     id int(8) PRIMARY KEY AUTO_INCREMENT,
@@ -18,3 +7,36 @@ CREATE TABLE accounts(
     email varchar(32),
     password varchar(32)
 );
+
+CREATE TABLE customer(
+    id int(8) PRIMARY KEY AUTO_INCREMENT,
+    name varchar(32),
+    address varchar(128)
+);
+
+CREATE TABLE orders (
+    id int(8) PRIMARY KEY AUTO_INCREMENT,
+    customer_id int(8),
+    created varchar(64),
+    shipped varchar(64),
+    FOREIGN KEY (customer_id) REFERENCES customer(id)
+);
+
+CREATE TABLE order_details (
+    order_id int(8) PRIMARY KEY AUTO_INCREMENT,
+    product_id int(8),
+    quantity int(8),
+    FOREIGN KEY (order_id) REFERENCES orders(id),
+    FOREIGN KEY (product_id) REFERENCES products(id)
+);
+
+CREATE TABLE products (
+    id int(8) PRIMARY KEY AUTO_INCREMENT,
+    productname varchar(32),
+    price varchar(32),
+    productimage varchar(128)
+);
+
+
+INSERT INTO `products` (`id`, `productname`, `price`, `productimage`) VALUES (NULL, 'MazeClan2019 Black Hoodie', '50$', 'images/products/shirt1.png');
+INSERT INTO `products` (`id`, `productname`, `price`, `productimage`) VALUES (NULL, 'MazeClan2019 TShirt', '30$', 'images/products/tshirt1.png'), (NULL, 'MaZeClan Red Hoodie', '50$', 'images/products/shirt.png');
